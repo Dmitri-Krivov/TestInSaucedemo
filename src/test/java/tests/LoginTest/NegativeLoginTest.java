689a2e2a0eda73f0be4d619d6e17f448b93b350a
DzmitryKryvou <qwertty1988@gmail.com>
2022-01-26 12:55:30 +0200

    Added Negative Test
    Added Negative Test,  Created SanityTest

--- src/test/java/tests/LoginTest/NegativeLoginTest.java
+++ src/test/java/tests/LoginTest/NegativeLoginTest.java
@@ -11,7 +11,19 @@ public class NegativeLoginTest  extends BaseTest {
     public void test_01() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.checkIn(Credentials.LOCKED_USER, Credentials.CORRECT_PASSWORD);
-
         Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Sorry, this user has been locked out.", "Address is incorrect");
     }
+    @Test(testName = "Login with empty user")
+    public void test_02() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.checkIn("", Credentials.CORRECT_PASSWORD);
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username is required", "Address is incorrect");
+    }
+    @Test(testName = "Login with empty password")
+    public void test_03() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.checkIn(Credentials.LOCKED_USER, "");
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required", "Address is incorrect");
+    }
+
 }
---
+++ src/test/java/tests/Sanity/SanityTest.java
@@ -0,0 +1,17 @@
+package tests.Sanity;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.Credentials;
+import pageObjects.LoginPage;
+import pageObjects.ProductsPage;
+import pageObjects.Urls;
+import tests.BaseTest;
+
+public class SanityTest extends BaseTest {
+
+    @Test(testName = "Sanity")
+    public void test_01() {
+
+    }
+}
